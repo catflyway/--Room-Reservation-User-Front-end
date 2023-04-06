8d31fcfa8feadc6de45ca044437b280b53ebb7d5
Suphamas Somsaipol <suphamas572@gmail.com>
2023-04-07 01:04:33 +0700

    allowrole

--- src/App.js
+++ src/App.js
@@ -1,79 +1,93 @@
-import React, { useState} from 'react';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Navbar from './conponents/Navbar';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./conponents/Navbar";
+import "./App.css";
 import axios from "axios";
 
-import Home from './pages/Home';
-import Room from './pages/Room';
-import Create from './pages/Create';
-import History from './pages/History/History';
-import Profile from './pages/Profile';
-import LoginForm from './conponents/LoginForm';
+import Home from "./pages/Home";
+import Room from "./pages/Room";
+import Create from "./pages/Create";
+import History from "./pages/History/History";
+import Profile from "./pages/Profile";
+import LoginForm from "./conponents/LoginForm";
+import RegisterForm from "./conponents/RegisterForm";
 
+import { UserContext } from "./user-context";
+
+const allowRole = ["User", "Room Contributor"];
 
 function App() {
-  const [userlogin, setUserlogin] = useState(()=>{
+  const [userlogin, setUserlogin] = useState(() => {
     let userProfle = localStorage.getItem("userData");
     if (userProfle) {
       let toto = JSON.parse(userProfle);
-      axios.defaults.headers.common['Authorization'] = `Bearer ${toto.token}`;
-      return JSON.parse(userProfle);
+      if (allowRole.includes(toto.role)) {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${toto.token}`;
+        return JSON.parse(userProfle);
+      }
+      localStorage.removeItem("userData");
     }
-    return {email: ""};
-    });
-  
-    
-    const [error, setError] = useState("");
-  
-    const Loginuser = (detailslogin) => {
-      console.log(detailslogin);
-      axios
-        .post("/auth/login", detailslogin)
-        
-        .then((response) => {
-            localStorage.setItem('userData', JSON.stringify(response.data));
-          console.log(response);
-        if (response.data.role=="User"||response.data.role=="Room Contributor"&&response.status == 200) {
-            console.log("Logged in");
-            axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
-            setUserlogin({
-              email: detailslogin.email,
-            });
-          }
-          else if(response.data.role!="User"){
-            console.log(response.data.role)
-            setError("Incorrect email or password");
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          console.log("Details do not match!");
-          setError("Incorrect email or password");
-        });
-    };
+    return { email: "" };
+  });
+
+  const [error, setError] = useState("");
+
+  const Loginuser = (detailslogin) => {
+    console.log(detailslogin);
+    axios
+      .post("/auth/login", detailslogin)
+
+      .then((response) => {
+        console.log(response);
+        if (response.status != 200) {
+          throw "Response not 200";
+        }
+
+        if (allowRole.includes(response.data.role)) {
+          console.log("Logged in");
+          axios.defaults.headers.common[
+            "Authorization"
+          ] = `Bearer ${response.data.token}`;
+          setUserlogin({
+            email: detailslogin.email,
+          });
+
+          localStorage.setItem("userData", JSON.stringify(response.data));
+        } else {
+          console.log(response.data.role);
+          setError("ไม่อยากให้เข้าอะ");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("Details do not match!");
+        setError("Incorrect email or password");
+      });
+  };
   return (
-    <>
-    <div className="App">
-    {userlogin.email != "" ? (
-      <>
-       <BrowserRouter>
-       <Navbar />
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/Rooms' element={<Room/>} />
-          <Route path='/Create' element={<Create/>} />
-          <Route path='/History' element={<History/>} />
-          <Route path='/Profiles' element={<Profile/>} />
-      
+    <UserContext.Provider value={userlogin}>
+      {userlogin.email != "" ? (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Rooms" element={<Room />} />
+            <Route path="/Create" element={<Create />} />
+            <Route path="/History" element={<History />} />
+            <Route path="/Profiles" element={<Profile />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
-      </BrowserRouter>
-      </>
-     ) : (
-      <LoginForm Loginuser={Loginuser} error={error} />
-    )}
-    </div>
-    </>
+        </BrowserRouter>
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginForm Loginuser={Loginuser} error={error} />} />
+            <Route path="/register" element={<RegisterForm/>} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </UserContext.Provider>
   );
 }
 
--- src/conponents/LoginForm.js
+++ src/conponents/LoginForm.js
@@ -1,57 +1,84 @@
-import React,{useState} from 'react'
-import profile from './image/a.jpg'
+import React, { useState } from "react";
+import profile from "./image/a.jpg";
 import { FiMail } from "react-icons/fi";
 import { FiLock } from "react-icons/fi";
 import { Button } from "antd";
-import RegisterForm from './RegisterForm';
+import RegisterForm from "./RegisterForm";
 
-function LoginForm({Loginuser,error}) {
-    const [detailslogin,setDetailslogin]=useState({name:"",email:"",password:""});
-    const submitHandler = e =>{
-        e.preventDefault();
+function LoginForm({ Loginuser, error }) {
+  const [detailslogin, setDetailslogin] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-        Loginuser(detailslogin); 
-    }
-    const OnButtonClick = () => {
-      <RegisterForm/>
-      console.log("Button clicked");
-    };
+    Loginuser(detailslogin);
+  };
+  const OnButtonClick = () => {
+    <RegisterForm />;
+    console.log("Button clicked");
+  };
   return (
-    <div className='main'>
-    <form>
-        <div className='form-inner'>
-            <h2>User Login</h2>
-            {(error!="")?(<div className='error'>{error}</div>) : ""}
-            <div className="imgs">
-          <div className="container-image">
-            <img src={profile} alt="profile" className="profile"/>
+    <div className="main">
+      <form>
+        <div className="form-inner">
+          <h2>User Login</h2>
+          {error != "" ? <div className="error">{error}</div> : ""}
+          <div className="imgs">
+            <div className="container-image">
+              <img src={profile} alt="profile" className="profile" />
+            </div>
           </div>
-        </div>
 
-            <div className='form-group'>
-                <label htmlFor='email'><h4>Email</h4> </label>
-                <div className='login-icon'>
-                <FiMail/>
-                </div>
-                <input type="email" placeholder="Email" name="email" id="email" onChange={e=>setDetailslogin({...detailslogin,email:e.target.value})} value={detailslogin.email}/>
+          <div className="form-group">
+            <label htmlFor="email">
+              <h4>Email</h4>{" "}
+            </label>
+            <div className="login-icon">
+              <FiMail />
             </div>
-            <div className='form-group'>
-                <label htmlFor='password'> <h4>Password:</h4></label>
-                <div className='login-icon'>
-                <FiLock/>
-                </div>
-                <input type="password" placeholder="password" name="password" id="password" onChange={e=>setDetailslogin({...detailslogin,password:e.target.value})} value={detailslogin.password}/>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              id="email"
+              onChange={(e) =>
+                setDetailslogin({ ...detailslogin, email: e.target.value })
+              }
+              value={detailslogin.email}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">
+              {" "}
+              <h4>Password:</h4>
+            </label>
+            <div className="login-icon">
+              <FiLock />
             </div>
-            <Button className='button-login' onClick={submitHandler}>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              id="password"
+              onChange={(e) =>
+                setDetailslogin({ ...detailslogin, password: e.target.value })
+              }
+              value={detailslogin.password}
+            />
+          </div>
+          <Button className="button-login" size="large" onClick={submitHandler}>
             Login
           </Button>
-          <Button className='button-reg'  onClick={OnButtonClick}>
-          Register
+          <Button className="button-reg" href="/register" size="large">
+            Register
           </Button>
         </div>
-    </form>
+      </form>
     </div>
-  )
+  );
 }
 
 export default LoginForm;
