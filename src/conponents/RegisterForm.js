15101a8e78688a0a19a367eb19ab49274c61ab6a
Suphamas Somsaipol <suphamas572@gmail.com>
2023-04-07 01:05:17 +0700

    add register

--- src/conponents/RegisterForm.js
+++ src/conponents/RegisterForm.js
@@ -1,19 +1,118 @@
-import React,{useState} from 'react'
-import profile from './image/a.jpg'
-import { FiMail } from "react-icons/fi";
-import { FiLock } from "react-icons/fi";
-import { Button } from "antd";
-
+import React, { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Cascader,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Select,
+  Switch,
+  TreeSelect,
+  Upload,
+  Row,
+  Col,
+} from "antd";
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
 function RegisterForm() {
-
   return (
-    <div className='App'>
-    <p>
-      ผ่านค้าาาาาาาาาาาาา
-    </p>
-   
+    <div className="User-list">
+      <div className="Heard-Manageano">
+        <h1>RegisterForm</h1>
+      </div>
+      <Row>
+        <Col span={12} offset={6}>
+          <Form
+            labelCol={{
+              span: 6,
+            }}
+            wrapperCol={{
+              span: 18,
+            }}
+            layout="horizontal"
+            style={{
+              maxWidth: 600,
+            }}
+          >
+            <Form.Item label="Input">
+              <Input />
+            </Form.Item>
+            <Form.Item label="Select">
+              <Select>
+                <Select.Option value="demo">Demo</Select.Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="TreeSelect">
+              <TreeSelect
+                treeData={[
+                  {
+                    title: "Light",
+                    value: "light",
+                    children: [
+                      {
+                        title: "Bamboo",
+                        value: "bamboo",
+                      },
+                    ],
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="Cascader">
+              <Cascader
+                options={[
+                  {
+                    value: "zhejiang",
+                    label: "Zhejiang",
+                    children: [
+                      {
+                        value: "hangzhou",
+                        label: "Hangzhou",
+                      },
+                    ],
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="DatePicker">
+              <DatePicker />
+            </Form.Item>
+            <Form.Item label="RangePicker">
+              <RangePicker />
+            </Form.Item>
+            <Form.Item label="InputNumber">
+              <InputNumber />
+            </Form.Item>
+            <Form.Item label="TextArea">
+              <TextArea rows={4} />
+            </Form.Item>
+            <Form.Item label="Switch" valuePropName="checked">
+              <Switch />
+            </Form.Item>
+            <Form.Item label="Upload" valuePropName="fileList">
+              <Upload action="/upload.do" listType="picture-card">
+                <div>
+                  <PlusOutlined />
+                  <div
+                    style={{
+                      marginTop: 8,
+                    }}
+                  >
+                    Upload
+                  </div>
+                </div>
+              </Upload>
+            </Form.Item>
+            <Form.Item label="Button">
+              <Button>Button</Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
     </div>
-  )
+  );
 }
 
 export default RegisterForm;
