c605e40eb401613448abf8bfaef26b6a6fe64881
suphamas <suphamas572@gmail.com>
2023-04-10 23:29:14 +0700

    post register

--- src/conponents/RegisterForm.js
+++ src/conponents/RegisterForm.js
@@ -1,117 +1,355 @@
-import React, { useState } from "react";
-import { PlusOutlined } from "@ant-design/icons";
+import React, { useState, useEffect } from "react";
 import {
-  Button,
-  Cascader,
-  DatePicker,
-  Form,
+  Modal,
   Input,
-  InputNumber,
+  Typography,
+  Form,
   Select,
-  Switch,
-  TreeSelect,
-  Upload,
-  Row,
+  Checkbox,
   Col,
+  Row,
+  message,
+  Upload,
+  Button,
+  Space,
 } from "antd";
-const { RangePicker } = DatePicker;
-const { TextArea } = Input;
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { UploadOutlined } from "@ant-design/icons";
+import { PlusOutlined } from "@ant-design/icons";
+import axios from "axios";
+import LoginForm from "./LoginForm";
+
+const { Title } = Typography;
 function RegisterForm() {
-  return (
-    <div className="User-list">
-      <div className="Heard-Manageano">
-        <h1>RegisterForm</h1>
+  const [orgList, setOrgList] = useState([]);
+  const [orgLoading, setOrgLoading] = useState(false);
+  function getOrg() {
+    setOrgLoading(true);
+    axios
+      .get("/org")
+      .then((response) => {
+        setOrgLoading(false);
+        console.log(response);
+        setOrgList(response.data);
+      })
+      .catch((err) => {
+        setOrgLoading(false);
+      });
+  }
+  const [userStatusLoading, setUserStatusLoading] = useState(false);
+  const [status, setstatus] = useState([]);
+  function getStatus(id) {
+    setUserStatusLoading(true);
+    axios
+      .get("/org/status/" + id)
+      .then((response) => {
+        setUserStatusLoading(false);
+        console.log(response);
+        setstatus(response.data);
+
+        //   form.resetFields(["status"])
+      })
+      .catch((err) => {
+        setUserStatusLoading(false);
+      });
+  }
+  const getBase64 = (img, callback) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => callback(reader.result));
+    reader.readAsDataURL(img);
+  };
+  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
+  const beforeUpload = (file) => {
+    const isLt10M = file.size / 1024 / 1024 < 10;
+
+    if (!isLt10M) {
+      message.error("Image must smaller than 10MB!");
+      return false;
+    }
+
+    getBase64(file, (url) => {
+      setImageUrl(url);
+    });
+    return false;
+  };
+
+  const normFile = (e) => {
+    console.log("Upload event:", e);
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e?.fileList;
+  };
+  const onAddUser = (e) => {
+    form.submit();
+  };
+  const onCancelAdd = () => {
+    setIsModalOpen(false);
+  };
+
+  const showModal = () => {
+    form.resetFields();
+    setImageUrl(undefined);
+    setLoading(false);
+    setIsModalOpen(true);
+  };
+
+  const onChangeorg = (orgID) => {
+    console.log(`selected ${orgID}`);
+    getStatus(orgID);
+  };
+
+  const onChangestatus = (statusID) => {
+    console.log(`selected ${statusID}`);
+  };
+
+  useEffect(() => {
+    getOrg();
+  }, []);
+
+  const [Clickcreate, setClickcreate] = useState(true);
+  const Clicknext = (e) => {
+    setClickcreate(true);
+    console.log("5555");
+  };
+  const onFormFinish = (formValue) => {
+    // formValue = {
+    //   ...formValue,
+    //   image: formValue.image[0].originFileObj,
+    // };
+    // axios
+    //   .postForm("/auth/register", formValue)
+    //   .then((response) => {
+    //     console.log("res", response);
+    //     setLoading(false);
+    //     setIsModalOpen(false);
+    //     message.success("สมัครสมาชิกสำเร็จ");
+    //     setClickcreate(false);
+    //   })
+    //   .catch((err) => {
+    //     console.log("err", err);
+    //     setLoading(false);
+    //     message.error("ERROR");
+    //   });
+    setClickcreate(false);
+    console.log(formValue);
+  };
+  const uploadButton = (
+    <div>
+      {loading ? <UploadOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
       </div>
-      <Row>
-        <Col span={12} offset={6}>
-          <Form
-            labelCol={{
-              span: 6,
-            }}
-            wrapperCol={{
-              span: 18,
-            }}
-            layout="horizontal"
-            style={{
-              maxWidth: 600,
-            }}
-          >
-            <Form.Item label="Input">
-              <Input />
-            </Form.Item>
-            <Form.Item label="Select">
-              <Select>
-                <Select.Option value="demo">Demo</Select.Option>
-              </Select>
-            </Form.Item>
-            <Form.Item label="TreeSelect">
-              <TreeSelect
-                treeData={[
-                  {
-                    title: "Light",
-                    value: "light",
-                    children: [
-                      {
-                        title: "Bamboo",
-                        value: "bamboo",
-                      },
-                    ],
-                  },
-                ]}
-              />
-            </Form.Item>
-            <Form.Item label="Cascader">
-              <Cascader
-                options={[
-                  {
-                    value: "zhejiang",
-                    label: "Zhejiang",
-                    children: [
-                      {
-                        value: "hangzhou",
-                        label: "Hangzhou",
-                      },
-                    ],
-                  },
-                ]}
-              />
-            </Form.Item>
-            <Form.Item label="DatePicker">
-              <DatePicker />
-            </Form.Item>
-            <Form.Item label="RangePicker">
-              <RangePicker />
-            </Form.Item>
-            <Form.Item label="InputNumber">
-              <InputNumber />
-            </Form.Item>
-            <Form.Item label="TextArea">
-              <TextArea rows={4} />
-            </Form.Item>
-            <Form.Item label="Switch" valuePropName="checked">
-              <Switch />
-            </Form.Item>
-            <Form.Item label="Upload" valuePropName="fileList">
-              <Upload action="/upload.do" listType="picture-card">
-                <div>
-                  <PlusOutlined />
-                  <div
-                    style={{
-                      marginTop: 8,
-                    }}
-                  >
-                    Upload
-                  </div>
-                </div>
-              </Upload>
-            </Form.Item>
-            <Form.Item label="Button">
-              <Button>Button</Button>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
     </div>
+  );
+  return (
+    <>
+      {Clickcreate == true ? (
+        <div className="User-list">
+          <Row justify="center">
+            <Col>
+              <Title style={{ color: " #3F478D" }}>RegisterForm</Title>
+            </Col>
+          </Row>
+          <br />
+
+          <Row justify="center">
+            <Col span={18} offset={6}>
+              <Form
+                form={form}
+                labelCol={{
+                  span: 4,
+                }}
+                wrapperCol={{
+                  span: 12,
+                }}
+                layout="horizontal"
+                onFinish={onFormFinish}
+                disabled={loading}
+              >
+                <Form.Item
+                  name="image"
+                  rules={[{ required: true }]}
+                  label=""
+                  valuePropName="fileList"
+                  getValueFromEvent={normFile}
+                  wrapperCol={{ span: 16 }}
+                >
+                  <Upload
+                    accept="image/*"
+                    listType="picture-card"
+                    showUploadList={false}
+                    beforeUpload={beforeUpload}
+                    maxCount={1}
+                    className="form-upload-picture"
+                  >
+                    {imageUrl ? (
+                      <div style={{ width: "100%" }}>
+                        <img
+                          src={imageUrl}
+                          alt="avatar"
+                          style={{
+                            maxHeight: "150px",
+                            marginBottom: "8px",
+                          }}
+                        />
+                      </div>
+                    ) : (
+                      uploadButton
+                    )}
+                    {/* <Button icon={<UploadOutlined />}>Click to upload</Button> */}
+                  </Upload>
+                </Form.Item>
+
+                <Form.Item
+                  label="หน่วยงาน"
+                  name="org"
+                  rules={[{ required: true, whitespace: true }]}
+                >
+                  <Select
+                    showSearch
+                    placeholder="หน่วยงาน"
+                    optionFilterProp="children"
+                    onChange={onChangeorg}
+                    filterOption={(input, option) =>
+                      (option?.name ?? "")
+                        .toLowerCase()
+                        .includes(input.toLowerCase())
+                    }
+                    fieldNames={{ label: "name", value: "_id" }}
+                    options={orgList}
+                    loading={orgLoading}
+                  />
+                </Form.Item>
+                <Form.Item
+                  label="Username"
+                  name="username"
+                  rules={[
+                    { min: 6, max: 12, required: true, whitespace: true },
+                  ]}
+                  hasFeedback
+                >
+                  <Input placeholder="Username" />
+                </Form.Item>
+                <Form.Item
+                  label="Firstname"
+                  name="firstname"
+                  rules={[
+                    { min: 4, max: 25, required: true, whitespace: true },
+                  ]}
+                >
+                  <Input placeholder="Firstname" />
+                </Form.Item>
+                <Form.Item
+                  label="Lastname"
+                  name="lastname"
+                  rules={[
+                    { min: 6, max: 25, required: true, whitespace: true },
+                  ]}
+                >
+                  <Input placeholder="Lastname" />
+                </Form.Item>
+                <Form.Item
+                  label="E-mail"
+                  name="email"
+                  validateFirst={true}
+                  rules={[
+                    {
+                      min: 6,
+                      max: 25,
+                      required: true,
+                      type: "email",
+                      whitespace: true,
+                    },
+                    {
+                      validator: (_, value) => {
+                        console.log(value);
+                        return new Promise((resolve) => {
+                          setTimeout(() => {
+                            console.log(value, "ok");
+                            resolve();
+                          }, 1000);
+                        });
+                      },
+
+                      message: "มีคนใช้แล้ว นะจ่ะ",
+                    },
+                  ]}
+                  hasFeedback
+                >
+                  <Input placeholder="E-mail" />
+                </Form.Item>
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[
+                    { min: 6, max: 25, required: true, whitespace: true },
+                  ]}
+                >
+                  <Input.Password placeholder="Password" />
+                </Form.Item>
+
+                <Form.Item
+                  label="Status"
+                  name="status"
+                  rules={[{ required: true, whitespace: true }]}
+                >
+                  <Select
+                    showSearch
+                    placeholder="Status"
+                    optionFilterProp="children"
+                    onChange={onChangestatus}
+                    filterOption={(input, option) =>
+                      (option?.name ?? "")
+                        .toLowerCase()
+                        .includes(input.toLowerCase())
+                    }
+                    fieldNames={{ label: "name", value: "_id" }}
+                    options={status}
+                    loading={userStatusLoading}
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  label="Role"
+                  name="role"
+                  rules={[{ required: true, whitespace: true }]}
+                >
+                  <Select placeholder="Select a Role">
+                    <Select.Option value="User">User</Select.Option>
+                    {/* <Select.Option value="Room Contributor">
+                  Room Contributor
+                </Select.Option>
+                <Select.Option value="Contributor">Contributor</Select.Option>
+                <Select.Option value="Administrator">
+                  Administrator
+                </Select.Option> */}
+                  </Select>
+                </Form.Item>
+                <Row justify="center">
+                  <Col span={6}>
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit">
+                        สร้างการจอง
+                      </Button>
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </Form>
+            </Col>
+          </Row>
+        </div>
+      ) : (
+        <RegisterForm />
+      )}
+    </>
   );
 }
 
