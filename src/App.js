b6e7244980ec50b3a2f87b3321e92101ad6134a4
Suphamas Somsaipol <suphamas572@gmail.com>
2023-04-24 18:17:17 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
 import React, { useState,useEffect } from "react";
+=======
+import React, { useState, useEffect } from "react";
+>>>>>>> 6a232c9d29eb652792398fff4226c17fa4613219
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./conponents/Navbar";
 import "./App.css";
 import axios from "axios";
+<<<<<<< HEAD
 import { Layout, ConfigProvider ,Spin, message} from "antd";
+=======
+import { Layout, ConfigProvider, Spin, message } from "antd";
+>>>>>>> 6a232c9d29eb652792398fff4226c17fa4613219
 
 import { MenuItems } from "./conponents/MenuItems";
 
@@ -19,6 +27,7 @@ const allowRole = ["User", "Room Contributor"];
 function App() {
   const [loadingCount, setLoadingCount] = useState(0);
 
+<<<<<<< HEAD
 useEffect(() => {
   // Add a request interceptor
   axios.interceptors.request.use(function (config) {
@@ -45,6 +54,33 @@ useEffect(() => {
   });
 }, []);
 
+=======
+  useEffect(() => {
+    // Add a request interceptor
+    axios.interceptors.request.use(function (config) {
+      // Do something before request is sent
+      setLoadingCount((state, props) => (state + 1))
+      return config;
+    }, function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    });
+
+    // Add a response interceptor
+    axios.interceptors.response.use(function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      setLoadingCount((state, props) => (state - 1))
+      return response;
+    }, function (error) {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      setLoadingCount((state, props) => (state - 1))
+      message.error(error)
+      return Promise.reject(error);
+    });
+  }, []);
+>>>>>>> 6a232c9d29eb652792398fff4226c17fa4613219
   const [userlogin, setUserlogin] = useState(() => {
     let userProfle = localStorage.getItem("userData");
     if (userProfle) {
@@ -103,6 +139,7 @@ useEffect(() => {
           },
         }}
       >
+<<<<<<< HEAD
       <Spin size="large" tip="Loading..." spinning={loadingCount !== 0}>
         {userlogin.email !== "" ? (
           <BrowserRouter>
@@ -118,37 +155,55 @@ useEffect(() => {
                 <div
                   className="site-layout-content"
                   style={{ background: "#FFF" }}
+=======
+        <Spin size="large" tip="Loading..." spinning={loadingCount !== 0}>
+          {userlogin.email !== "" ? (
+            <BrowserRouter>
+              <Layout className="layout">
+                <Header>
+                  <Navbar />
+                </Header>
+                <Content
+                  style={{
+                    padding: "0 50px",
+                  }}
+>>>>>>> 6a232c9d29eb652792398fff4226c17fa4613219
                 >
-                  <Routes>
-                    {MenuItems.map((item, index) => {
-                      if (!item.role.includes(userlogin.role)) {
-                        return undefined;
-                      }
-                      return (
-                        <Route
-                          key={index}
-                          path={item.path}
-                          element={item.element}
-                        />
-                      );
-                    })}
-                    <Route
-                      path="*"
-                      element={
-                        // <Navigate to={UserDefaultPage[userlogin.role]} replace />
-                        <Navigate to="/" replace />
-                      }
-                    />
-                    {/* <Route path="/" element={<Home />} />
+                  <div
+                    className="site-layout-content"
+                    style={{ background: "#FFF" }}
+                  >
+                    <Routes>
+                      {MenuItems.map((item, index) => {
+                        if (!item.role.includes(userlogin.role)) {
+                          return undefined;
+                        }
+                        return (
+                          <Route
+                            key={index}
+                            path={item.path}
+                            element={item.element}
+                          />
+                        );
+                      })}
+                      <Route
+                        path="*"
+                        element={
+                          // <Navigate to={UserDefaultPage[userlogin.role]} replace />
+                          <Navigate to="/" replace />
+                        }
+                      />
+                      {/* <Route path="/" element={<Home />} />
             <Route path="/Rooms" element={<Room />} />
             <Route path="/Create" element={<Create />} />
             <Route path="/History" element={<History />} />
             <Route path="/Profiles" element={<Profile />} />
             <Route path="*" element={<Navigate to="/" replace />} /> */}
-                  </Routes>
-                </div>
-              </Content>
+                    </Routes>
+                  </div>
+                </Content>
 
+<<<<<<< HEAD
               <Footer
                 style={{
                   textAlign: "center",
@@ -168,6 +223,27 @@ useEffect(() => {
             </Routes>
           </BrowserRouter>
         )}
+=======
+                <Footer
+                  style={{
+                    textAlign: "center",
+                  }}
+                ></Footer>
+              </Layout>
+            </BrowserRouter>
+          ) : (
+            <BrowserRouter>
+              <Routes>
+                <Route
+                  path="/"
+                  element={<LoginForm Loginuser={Loginuser} error={error} />}
+                />
+                <Route path="/register" element={<RegisterForm />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
+            </BrowserRouter>
+          )}
+>>>>>>> 6a232c9d29eb652792398fff4226c17fa4613219
         </Spin>
       </ConfigProvider>
     </UserContext.Provider>
